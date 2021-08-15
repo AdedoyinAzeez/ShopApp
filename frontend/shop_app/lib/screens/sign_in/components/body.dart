4b6ad1a7220f6d83f409650139709bd05669d451
Adedoyin Azeez <adedoyinazeez00@gmail.com>
2021-08-15 21:04:58 +0100

    modified body

--- frontend/shop_app/lib/screens/sign_in/components/body.dart
+++ frontend/shop_app/lib/screens/sign_in/components/body.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shop_app/components/custom_suffix_icon.dart';
-import 'package:shop_app/components/default_button.dart';
-import 'package:shop_app/components/form_error.dart';
-import 'package:shop_app/constants.dart';
 import 'package:shop_app/screens/sign_in/components/sign_form.dart';
 import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SizedBox(
         width: double.infinity,
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
           child: Column(
             children: [
               Text(
@@ -38,4 +33,3 @@ class Body extends StatelessWidget {
     );
   }
 }
-
